02f12b6a62291c375e6fb09d88b730dd1ae4f646
Zaemon <contact@zaemon.jp>
2024-12-06 00:14:24 +0900

    fix(database) remove database

--- src/main/java/world/anhgelus/gamelibrary/database/redis/RedisCredentials.java
+++
@@ -1,9 +0,0 @@
-package world.anhgelus.gamelibrary.database.redis;
-
-/**
- * Redis credentials
- * @param host Host of the redis server (e.g: localhost)
- * @param port Port of the redis server
- */
-public record RedisCredentials(String host, int port) {
-}
--- src/main/java/world/anhgelus/gamelibrary/database/redis/RedisException.java
+++
@@ -1,7 +0,0 @@
-package world.anhgelus.gamelibrary.database.redis;
-
-public class RedisException extends Exception{
-    public RedisException(String message) {
-        super(message);
-    }
-}
